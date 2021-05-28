815c03fc25b36fd3b66c9e90bc2c625ef73460b4
piyoppi <garakuta.toolbox@gmail.com>
2021-05-28 21:58:31 +0900

    REACT_APP PrefixがないのでDEVELOPMENT_MODE環境変数が意図せず適用されない不具合を修正する

--- packages/forms/src/config.ts
+++ packages/forms/src/config.ts
@@ -3,6 +3,6 @@ import { transformErrors } from './lib/errorTransformer/ja'
 export const config = {
   baseUrl: process.env.REACT_APP_BASE_URL || 'http://localhost',
   schemaBaseUrl: process.env.REACT_APP_SCHEMA_BASE_URL || 'http://localhost',
-  developmentMode: process.env.DEVELOPMENT_MODE === 'true' ?  true : false,
+  developmentMode: process.env.REACT_APP_DEVELOPMENT_MODE === 'true' ?  true : false,
   errorTransformer: transformErrors
 }
